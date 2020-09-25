75aec375ec7ce9908a73b7e231bf1997c35d34be
Paulius Stundzia <paulius.stundzia@oxylabs.io>
2020-09-25 13:49:23 +0300

    Update proxy

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -1,16 +1,35 @@
 package proxy
 
 import (
+	"fmt"
+	"github.com/stundzia/scrapeutils/db"
+	"go.uber.org/zap"
 	"log"
 	"net/http"
 	"net/url"
 )
 
 type Pool struct {
-	proxies map[*Proxy]int
+	proxies map[*proxy]int
+	proxyDB *db.ProxyDB
+	logger *zap.Logger
 }
 
-func (pp *Pool) getRandomProxy() *Proxy {
+type proxy struct {
+	urlStr string
+	url *url.URL
+}
+
+
+func NewProxyPool(logger *zap.Logger) *Pool {
+	return &Pool{
+		proxies: make(map[*proxy]int),
+		proxyDB: nil,
+		logger:  logger,
+	}
+}
+
+func (pp *Pool) GetRandomProxy() *proxy {
 	for p := range pp.proxies {
 		pp.proxies[p]++
 		return p
@@ -22,28 +41,44 @@ func (pp *Pool) AddProxyViaUrlString(s string) {
 	pp.proxies[GetProxyFromUrlString(s)] = 0
 }
 
-type Proxy struct {
-	urlStr string
-	url *url.URL
-}
-
-func (p *Proxy) GetTransport() *http.Transport {
+func (p *proxy) GetTransport() *http.Transport {
 	transport := &http.Transport{
 		Proxy: http.ProxyURL(p.url),
 	}
 	return transport
 }
 
-func GetProxyFromUrlString(s string) (proxy *Proxy) {
-	proxy = &Proxy{
+func GetProxyFromUrlString(s string) (p *proxy) {
+	p = &proxy{
 		urlStr:   s,
 		url:      nil,
 	}
 	proxyURL, err := url.Parse(s)
 	if err != nil {
 		log.Fatal(err)
-		return proxy
+		return p
 	}
-	proxy.url = proxyURL
-	return proxy
+	p.url = proxyURL
+	return p
+}
+
+func (pp *Pool) AddProxyFromDBProxy(proxyRow *db.ProxyRow) {
+	var proxyUrl string
+	if len(proxyRow.Username) > 0 && len(proxyRow.Password) > 0 {
+		proxyUrl = fmt.Sprintf("http://%s:%s@%s:%d", proxyRow.Username, proxyRow.Password, proxyRow.Host, proxyRow.Port)
+	} else {
+		proxyUrl = fmt.Sprintf("http://%s:%s", proxyRow.Host, proxyRow.Port)
+	}
+	pp.AddProxyViaUrlString(proxyUrl)
+}
+
+func (pp *Pool) PopulatePoolFromDB(basesource string, proxyType string, limit int) {
+	pp.logger.Info("fetching proxies from DB", zap.Int("limit", limit), zap.String("basesource", basesource), zap.String("proxy type", proxyType))
+	proxieRows, err := pp.proxyDB.GetProxies(basesource, proxyType, limit)
+	if err != nil {
+		pp.logger.Error("proxy get from db error", zap.String("error", err.Error()))
+	}
+	for _, r := range proxieRows {
+		fmt.Println(r)
+	}
 }
